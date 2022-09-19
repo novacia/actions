babb25e8d8946c6f00e5b25124af84be066e8d6a
Silask1 <silasafram@gmail.com>
2022-09-19 16:13:35 -0300

    ajuste de parametros de input

--- build-assembly/src/main.ts
+++ build-assembly/src/main.ts
@@ -1,14 +1,21 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 
 const version = 7
 
 async function run(): Promise<void> {
     try {
         const build = core.getInput('build', { required: true })
+        const csproj = core.getInput('csproj', { required: true })
+        
+        console.log('build - Assembly')
 
-        core.debug('build - Assembly')
-        core.debug('Iniciando build')
-        core.debug('Finalizando build')
+        console.log('Build: ' + build)
+        console.log('csproj: ' + csproj)
+        console.log('GITHUB_RUN_NUMBER: ' + github.context.runNumber)
+
+        console.log('Iniciando build')
+        console.log('Finalizando build')
 
     } catch (error) {
         if (error instanceof Error) core.setFailed(error.message)

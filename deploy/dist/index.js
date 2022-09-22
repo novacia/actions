b70cfad84b2eefc347e2b50200b12f1c5c0ee4f2
Silask1 <silasafram@gmail.com>
2022-09-22 10:14:47 -0300

    Update index.js

--- deploy/dist/index.js
+++ deploy/dist/index.js
@@ -2673,175 +2673,6 @@ exports["default"] = _default;
 
 /***/ }),
 
-/***/ 2123:
-/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
-
-"use strict";
-
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-exports.__esModule = true;
-exports.sshComando = exports.sshGithub = void 0;
-var ssh2_1 = __nccwpck_require__(3353);
-var core = __nccwpck_require__(3820);
-var SSH2Promise = __nccwpck_require__(1058);
-var sshGithub = /** @class */ (function () {
-    function sshGithub(settings) {
-        if (!settings.password) {
-            this._config = {
-                host: settings.host,
-                port: settings.port,
-                username: settings.username,
-                privateKey: settings.key
-            };
-        }
-        else if (!settings.key) {
-            this._config = {
-                host: settings.host,
-                port: settings.port,
-                username: settings.username,
-                password: settings.password
-            };
-        }
-        this._ssh = new ssh2_1.Client();
-    }
-    sshGithub.prototype.comado = function (cmd) {
-        var _this = this;
-        this._ssh.on('ready', function () {
-            _this._ssh.exec(cmd, function (err, stream) {
-                if (err)
-                    throw new Error(err.message);
-                stream.on('data', function (data) {
-                    core.info('exec STDOUT: ' + data);
-                }).stderr.on('data', function (data) {
-                    throw new Error('exec STDOUT: ' + data);
-                }).on('close', function (code, signal) {
-                    core.info('Code: ' + code + ', Signal: ' + signal);
-                    _this._ssh.end();
-                });
-            });
-        }).on('error', function (err) {
-            core.info('Client SSH :: error: ' + err.message);
-        }).connect(this._config);
-    };
-    return sshGithub;
-}());
-exports.sshGithub = sshGithub;
-function sshComando(settings, cmd) {
-    return __awaiter(this, void 0, void 0, function () {
-        var config, ssh_1;
-        return __generator(this, function (_a) {
-            try {
-                if (!settings.password) {
-                    config = {
-                        host: settings.host,
-                        port: settings.port,
-                        username: settings.username,
-                        privateKey: settings.key
-                    };
-                }
-                else if (!settings.key) {
-                    config = {
-                        host: settings.host,
-                        port: settings.port,
-                        username: settings.username,
-                        password: settings.password
-                    };
-                }
-                ssh_1 = new ssh2_1.Client();
-                ssh_1.on('ready', function () {
-                    core.info('Client SSH :: conectado com sucesso');
-                    ssh_1.exec(cmd, function (err, stream) {
-                        if (err)
-                            throw new Error(err.message);
-                        stream.on('data', function (data) {
-                            core.info('exec STDOUT: ' + data);
-                        }).stderr.on('data', function (data) {
-                            throw new Error('exec STDOUT: ' + data);
-                        }).on('close', function (code, signal) {
-                            core.info('Code: ' + code + ', Signal: ' + signal);
-                            ssh_1.end();
-                        });
-                    });
-                }).on('error', function (err) {
-                    core.info('Client SSH :: error: ' + err.message);
-                }).connect(config).on('end', function () {
-                    core.info('Client SSH :: desconectado');
-                });
-            }
-            catch (error) {
-                throw new Error('sshComando :: error: ' + error.message);
-            }
-            return [2 /*return*/];
-        });
-    });
-}
-exports.sshComando = sshComando;
-function run() {
-    return __awaiter(this, void 0, void 0, function () {
-        var sshconfig, ssh;
-        return __generator(this, function (_a) {
-            sshconfig = {
-                host: '20.120.4.78',
-                port: 22,
-                username: 'tqssolucoes'
-            };
-            ssh = new SSH2Promise(sshconfig);
-            ssh.connect().then(function () {
-                core.info("Connection established");
-            });
-            core.info('Executar comando');
-            ssh.exec('ls -l').then(function (data) {
-                core.info(data);
-            });
-            core.info('Comando executado');
-            ssh.close().then(function () {
-                core.info('Desconection sucess');
-            });
-            core.info('Removendo stack GitFlow');
-            return [2 /*return*/];
-        });
-    });
-}
-run();
-
-
-/***/ }),
-
 /***/ 2674:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
@@ -51596,7 +51427,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__nccwpck_require__(2186));
 const contexto_1 = __nccwpck_require__(5517);
-const ssh = __importStar(__nccwpck_require__(2123));
+const ssh = __importStar(__nccwpck_require__(1208));
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
@@ -51692,6 +51523,90 @@ function getInputsDeploy() {
     };
 }
 exports.getInputsDeploy = getInputsDeploy;
+
+
+/***/ }),
+
+/***/ 1208:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+
+"use strict";
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.sshComando = void 0;
+const core = __importStar(__nccwpck_require__(3820));
+const ssh2_promise_1 = __importDefault(__nccwpck_require__(1058));
+function sshComando(settings, cmd) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            var config;
+            if (!settings.password) {
+                config = {
+                    host: settings.host,
+                    port: settings.port,
+                    username: settings.username,
+                    privateKey: settings.key
+                };
+            }
+            else if (!settings.key) {
+                config = {
+                    host: settings.host,
+                    port: settings.port,
+                    username: settings.username,
+                    password: settings.password
+                };
+            }
+            var ssh = new ssh2_promise_1.default(config);
+            ssh.connect()
+                .then(() => {
+                core.info('Conectado com sucesso');
+            });
+            ssh.exec(cmd)
+                .then((data) => {
+                core.info(data);
+            });
+        }
+        catch (error) {
+            throw new Error('sshComando :: error: ' + error.message);
+        }
+    });
+}
+exports.sshComando = sshComando;
 
 
 /***/ }),

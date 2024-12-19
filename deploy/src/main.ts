14a4aef59d87818ea7f744d672ad248c38c34e03
matpinsdorf <57017667+matpinsdorf@users.noreply.github.com>
2024-12-19 20:34:45 -0300

    Reapply "ajuste da action deploy para executar o script deploy.sh no host"
    This reverts commit 65fefed52f4a2ba3eb734570792698ecdf1472c2.

--- deploy/src/main.ts
+++ deploy/src/main.ts
@@ -29,11 +29,13 @@ async function run(): Promise<void> {
         let _caminhoDeploy: string = inputs.path == '' ? `./${inputs.stack}/docker-compose.yml` : `${inputs.path}/${inputs.stack}/docker-compose.yml`;
          
         if (inputs.latest) {
-            await ssh.sshComando(config, `${!inputs.omitir_sudo ? 'sudo' : ''} env ${_config} docker stack deploy -c ${_caminhoDeploy} ${stack_name}`);
+            // await ssh.sshComando(config, `${!inputs.omitir_sudo ? 'sudo' : ''} env ${_config} docker stack deploy -c ${_caminhoDeploy} ${stack_name}`);
+            await ssh.sshComando(config, `${!inputs.omitir_sudo ? 'sudo' : ''} env ${_config} deploy.sh ${stack_name} ${_caminhoDeploy} ${inputs.docker_username} ${inputs.docker_token}`);
         }
         else {
             var _versao = getVersao(inputs.versao_major, inputs.versao_minor, inputs.versao_patch, inputs.versao_patch_sufixo);
-            await ssh.sshComando(config, `${!inputs.omitir_sudo ? 'sudo' : ''} env ${_config} VERSAO=${_versao} docker stack deploy -c ${_caminhoDeploy} ${stack_name}`);
+            // await ssh.sshComando(config, `${!inputs.omitir_sudo ? 'sudo' : ''} env ${_config} VERSAO=${_versao} docker stack deploy -c ${_caminhoDeploy} ${stack_name}`);
+            await ssh.sshComando(config, `${!inputs.omitir_sudo ? 'sudo' : ''} env ${_config} VERSAO=${_versao} deploy.sh ${stack_name} ${_caminhoDeploy} ${inputs.docker_username} ${inputs.docker_token}`);
         }
         
         core.info('Finalizando Deploy');
